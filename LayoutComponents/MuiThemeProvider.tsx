86ec0b3c5e21ad093b56acdce06095b6bb5dc85f
aleksandarLazic1998 <aleksandar.lazic.p@gmail.com>
2023-09-16 14:05:18 +0200

    Set theme

--- LayoutComponents/MuiThemeProvider.tsx
+++ LayoutComponents/MuiThemeProvider.tsx
@@ -1,23 +1,23 @@
-'use client';
-import React, { ReactNode } from 'react';
-import { ThemeProvider } from '@mui/material/styles';
-import { CssBaseline, PaletteMode } from '@mui/material';
-import getDesignTokens from '@/src/utils/getDesignedTokens';
-import useCreateMUITheme from '@/hooks/useCreateMUITheme';
+"use client";
+import React, { ReactNode } from "react";
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline, PaletteMode } from "@mui/material";
+import getDesignTokens from "@/src/utils/getDesignedTokens";
+import useCreateMUITheme from "@/hooks/useCreateMUITheme";
 
 interface IProps {
-  children: ReactNode;
+	children: ReactNode;
 }
 
 const MuiThemeProvider = ({ children }: IProps) => {
-  const theme = useCreateMUITheme();
+	const theme = useCreateMUITheme();
 
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      {children}
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			{children}
+		</ThemeProvider>
+	);
 };
 
 export default MuiThemeProvider;
